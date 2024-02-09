0db322bb1bb3ab70892ef57139ae5e23f544f767
Code-XT <rahuljhabokaro123@gmail.com>
2024-02-09 19:20:41 +0530

    added middle sectrion buttons

--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,101 @@
 import { ModeToggle } from "@/components/toggle";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { FlipHorizontal } from "lucide-react";
+import {
+  CameraIcon,
+  FlipHorizontal,
+  PersonStanding,
+  Video,
+} from "lucide-react";
 import { useRef, useState } from "react";
+import { render } from "react-dom";
+import { Rings } from "react-loader-spinner";
 import Webcam from "react-webcam";
 export default function Home() {
   const camRef = useRef(null);
   const canvasRef = useRef(null);
-  const [mirrored, setMirrored] = useState(true);
+  const [mirrored, setMirrored] = useState(false);
+  const [isRecording, setIsRecording] = useState(false);
+  const [isAutoRecord, setIsAutoRecord] = useState(false);
+
+  const screenShot = () => {
+    if (!camRef.current) {
+      toast("Camera not found. Please refresh");
+    } else {
+      const imageSrc = camRef.current.getScreenshot();
+      const blob = base64toBlob(imageSrc);
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = `${formatDate(new Date())}.png`;
+      link.click();
+    }
+  };
+
+  const videoRecord = () => {
+    if (!camRef.current) {
+      toast("Camera not found. Please refresh");
+    } else {
+      setIsRecording((prev) => !prev);
+      if (isRecording) {
+        const imageSrc = camRef.current.getScreenshot();
+        const blob = base64toBlob(imageSrc);
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = `${formatDate(new Date())}.mp4`;
+        link.click();
+        setIsRecording(false);
+      }
+    }
+  };
+
+  const autoRecord = () => {
+    if (!camRef.current) {
+      toast("Camera not found. Please refresh");
+    } else {
+      setIsAutoRecord((prev) => !prev);
+      if (isAutoRecord) {
+        const imageSrc = camRef.current.getScreenshot();
+        const blob = base64toBlob(imageSrc);
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = `${formatDate(new Date())}.mp4`;
+        link.click();
+        setIsAutoRecord(false);
+      }
+    }
+  };
+
+  const base64toBlob = (base64) => {
+    const byteString = atob(base64.split(",")[1]);
+    const mimeString = base64.split(",")[0].split(":")[1].split(";")[0];
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    const blob = new Blob([ab], { type: mimeString });
+    return blob;
+  };
+
   return (
     <div className="flex h-screen">
       <div className="relative">
         <div className="relative h-screen w-full">
-          <Webcam ref={camRef} className="w-full h-full object-contain p-2" />
+          <Webcam
+            ref={camRef}
+            mirrored={mirrored}
+            screenshotFormat="image/jpeg"
+            videoConstraints={{
+              facingMode: "user",
+              width: 1280,
+              height: 720,
+              aspectRatio: 16 / 9,
+            }}
+            className="w-full h-full object-contain p-2"
+          />
           <canvas
             ref={canvasRef}
             className="absolute top-0 left-0 w-full h-full object-contain"
@@ -36,10 +119,50 @@ export default function Home() {
             <Separator />
           </div>
           <div className="flex flex-col gap-2">
-            <Separator>How Are</Separator>
+            <Separator />
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={() => {
+                screenShot;
+              }}
+            >
+              <CameraIcon />
+            </Button>
+            <Button
+              variant={isRecording ? "destructive" : "outline"}
+              size="icon"
+              onClick={() => {
+                videoRecord;
+              }}
+            >
+              <Video />
+            </Button>
+            <Button
+              variant={isAutoRecord ? "destructive" : "outline"}
+              size="icon"
+              onClick={() => {
+                autoRecord;
+              }}
+            >
+              {isAutoRecord ? (
+                <Rings
+                  visible={true}
+                  height="50"
+                  width="50"
+                  color="#4fa94d"
+                  ariaLabel="rings-loading"
+                  wrapperStyle={{}}
+                  wrapperClass=""
+                />
+              ) : (
+                <PersonStanding />
+              )}
+            </Button>
+            <Separator />
           </div>
           <div className="flex flex-col gap-2">
-            You
+            <Button></Button>
             <Separator />
           </div>
         </div>
